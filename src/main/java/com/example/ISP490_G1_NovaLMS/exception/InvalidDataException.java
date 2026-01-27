b62c264fa7e0629c81b3b468bbad24e6d9a5e54a
chutu14 <chutu1212141@gmail.com>
2026-01-27 15:10:44 +0700

    Initial upload for NovaLMS project

---
+++ src/main/java/com/example/ISP490_G1_NovaLMS/exception/InvalidDataException.java
@@ -0,0 +1,12 @@
+package com.example.DoAn.exception;
+
+public class InvalidDataException extends RuntimeException {
+
+    public InvalidDataException(String message) {
+        super(message);
+    }
+
+    public InvalidDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
