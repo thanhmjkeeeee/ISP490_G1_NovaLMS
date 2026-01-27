bfefa0d686c706199ddbd51b636342f0d92a2efa
ChiThanh <148964429+thanhmjkeeeee@users.noreply.github.com>
2026-01-27 15:35:35 +0700

    Delete src directory

--- src/main/java/com/example/ISP490_G1_NovaLMS/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.DoAn.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
