d018669aa868b38e0adda26499bf0d2e9a6c41c4
ChiThanh <148964429+xxxNartC@users.noreply.github.com>
2026-01-29 17:26:12 +0700

    up full code base

---
+++ NovaLMS/DoAn/src/main/java/com/example/DoAn/Controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.DoAn.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    // Khi người dùng vào trang chủ (http://localhost:8080/)
+    @GetMapping("/")
+    public String home() {
+        // Trả về tên file trong thư mục templates (không cần đuôi .html)
+        return "index";
+    }
+}
