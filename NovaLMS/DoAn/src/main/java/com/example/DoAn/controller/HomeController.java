24fe262810056dd0e8bba02a508dcbf69b233529
ChiThanh <148964429+xxxNartC@users.noreply.github.com>
2026-01-30 11:01:46 +0700

    change name file

--- NovaLMS/DoAn/src/main/java/com/example/DoAn/controller/HomeController.java
+++ NovaLMS/DoAn/src/main/java/com/example/DoAn/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.DoAn.Controller;
+package com.example.DoAn.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
