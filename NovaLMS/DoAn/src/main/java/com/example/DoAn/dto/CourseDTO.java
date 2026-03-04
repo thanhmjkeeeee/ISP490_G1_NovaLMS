b2dcea656e04ad03000addff74d6f85798ad1b46
chutu14 <chutu1212141@gmail.com>
2026-03-04 14:31:26 +0700

    fix

---
+++ NovaLMS/DoAn/src/main/java/com/example/DoAn/dto/CourseDTO.java
@@ -0,0 +1,19 @@
+package com.example.DoAn.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseDTO {
+    private Integer courseId;
+    private String courseCode;
+    private String courseName;
+    private String status;
+    private String description;
+    private Double price;
+}
