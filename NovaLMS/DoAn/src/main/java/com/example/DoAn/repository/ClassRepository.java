d39794ba5d3cbeddf3d826f0839c94ad36c57bc8
ducanh <he173374dinhhaducanh@gmail.com>
2026-03-07 16:40:53 +0700

    sửa lại view public class

--- NovaLMS/DoAn/src/main/java/com/example/DoAn/repository/ClassRepository.java
+++ NovaLMS/DoAn/src/main/java/com/example/DoAn/repository/ClassRepository.java
@@ -2,12 +2,13 @@ package com.example.DoAn.repository;
 
 import com.example.DoAn.model.Clazz;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ClassRepository extends JpaRepository<Clazz, Integer> {
+public interface ClassRepository extends JpaRepository<Clazz, Integer>, JpaSpecificationExecutor<Clazz> {
     // Tìm các lớp theo Course ID và trạng thái (ví dụ: tìm lớp đang mở 'Open')
     List<Clazz> findByCourse_CourseIdAndStatus(Integer courseId, String status);
 
