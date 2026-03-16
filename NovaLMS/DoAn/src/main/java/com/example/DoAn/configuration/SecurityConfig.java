f929bf58999a7d5f5259765a75cb581ccb47ceb1
chutu14 <chutu1212141@gmail.com>
2026-03-16 23:33:11 +0700

    update cors config

--- NovaLMS/DoAn/src/main/java/com/example/DoAn/configuration/SecurityConfig.java
+++ NovaLMS/DoAn/src/main/java/com/example/DoAn/configuration/SecurityConfig.java
@@ -97,7 +97,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cache-Control"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
