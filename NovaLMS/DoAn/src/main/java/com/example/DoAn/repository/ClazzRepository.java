f38e77c3f7a4a3106e1f1c53894c8ff9de2014f5
thanhmjkeeeee <maichithanh317@gmail.com>
2026-03-26 08:38:44 +0700

    fix quiz course bug in teacher

--- NovaLMS/DoAn/src/main/java/com/example/DoAn/repository/ClazzRepository.java
+++ NovaLMS/DoAn/src/main/java/com/example/DoAn/repository/ClazzRepository.java
@@ -2,8 +2,16 @@ package com.example.DoAn.repository;
 
 import com.example.DoAn.model.Clazz;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ClazzRepository extends JpaRepository<Clazz, Integer> {
+
+    // Lấy tất cả lớp mà giáo viên (theo email) đang phụ trách
+    @Query("SELECT c FROM Clazz c JOIN FETCH c.course WHERE c.teacher.userId = :teacherId")
+    List<Clazz> findAllByTeacher_UserId(@Param("teacherId") Integer teacherId);
 }
