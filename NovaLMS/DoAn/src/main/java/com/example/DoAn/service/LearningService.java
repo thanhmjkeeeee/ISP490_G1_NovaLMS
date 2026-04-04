d92e70200ca1bbf9e44886666441803cfd1e2a29
ChiThanh <148964429+xxxNartC@users.noreply.github.com>
2026-04-04 23:58:19 +0700

    Update: Student Dashboard

--- NovaLMS/DoAn/src/main/java/com/example/DoAn/service/LearningService.java
+++ NovaLMS/DoAn/src/main/java/com/example/DoAn/service/LearningService.java
@@ -1,8 +1,10 @@
 package com.example.DoAn.service;
 
+import com.example.DoAn.dto.response.ChartDataDTO;
 import com.example.DoAn.dto.response.CourseLearningInfoDTO;
 import com.example.DoAn.dto.response.ResponseData;
 
+import java.util.List;
 import java.util.Map;
 
 public interface LearningService {
@@ -10,4 +12,8 @@ public interface LearningService {
     ResponseData<Map<String, Object>> getLessonViewData(Integer lessonId, String email);
     ResponseData<Void> markLessonCompleted(Integer lessonId, String email);
     Integer getLessonIdToContinue(Integer courseId, String email);
+
+    // Study Time Tracking
+    ResponseData<Void> trackTime(String email, int seconds);
+    ResponseData<ChartDataDTO> getDashboardChartData(String email, int days);
 }
