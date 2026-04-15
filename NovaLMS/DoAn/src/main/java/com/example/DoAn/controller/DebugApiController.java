ea7c31acdd6bfa6dca4f298cc61d7bedea0a7a41
Ngoc Dat <ngocdat2003xxx@gmail.com>
2026-04-15 18:19:50 +0700

    Update password for all user

--- NovaLMS/DoAn/src/main/java/com/example/DoAn/controller/DebugApiController.java
+++ NovaLMS/DoAn/src/main/java/com/example/DoAn/controller/DebugApiController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/admin/debug")
